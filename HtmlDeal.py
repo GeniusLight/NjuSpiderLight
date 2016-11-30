a26d94e6b496c67592686b0842403a7aeb8bed6e
Light <18362927193@163.com>
2016-11-30 23:14:20 +0800

    初步的网页内容爬取，很粗略

---
+++ HtmlDeal.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+from bs4 import BeautifulSoup
+import json
+
+
+class SimpleTrs(object):
+    def __init__(self, title, content, url):
+        self.title = title
+        self.content = content
+        self.url = url
+
+class SimpleDeal(object):
+    def Method1(self, url, html_text, fp_error):
+        if html_text is None:
+            OutputS = SimpleTrs(None, None, url)
+            return OutputS
+
+        soup = BeautifulSoup(html_text, "lxml")
+        script = soup.find_all("script")  # 找到所有script的标签
+        for x in script:  # 判断script标签是否包含文本，若有，直接置为空
+            if x.string != None:
+                x.string.replace_with(" ")
+
+        try:
+            title = soup.title.string.strip()
+        except Exception as e:
+            fp_error.write(url + '\t' + str(e) + '\t' + "\n")  # 出现读出title为空的情况，不太明白
+            title = "None"
+
+        content = "".join(soup.stripped_strings)
+        content.replace('     ', ',')
+        OutputS = SimpleTrs(title, content, url)
+        return OutputS
---
+++ HtmlInput.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+from numpy import *
+import csv
+
+
+class InputUrl(object):
+    def ReadUrl(self, file):
+        CsvFile = open(file, 'rb')
+        CsvReader = csv.reader(CsvFile)
+        list = []
+        for row in CsvReader:
+            list.append(row[1])
+        return list
+
+    def ReadId(self, file):
+        CsvFile = open(file, 'rb')
+        CsvReader = csv.reader(CsvFile)
+        list = []
+        for row in CsvReader:
+            list.append(row[0])
+        return list
